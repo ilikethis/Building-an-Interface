6b8e71d32651bfc68661be6e949e7c205d94c46f
ilikethis <didanito@gmail.com>
2022-04-23 20:58:44 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import { useState, useEffect, useCallback } from 'react';
+import { BiCalendar } from 'react-icons/bi'
+import Search from './components/Search';
+import AddAppointment from './components/AddAppointment';
+import AppointmentInfo from './components/AppointmentInfo';
+
+const handleRemoveItem = (id, setAppointmentList, appointmentList) => {
+  setAppointmentList(appointmentList.filter(item => item.id !== id));
+}
+
+const App = () => {
+  const [appointmentList, setAppointmentList] = useState([]);
+  const [query, setQuery] = useState('');
+  const [sortBy, setSortBy] = useState('petName');
+  const [orderBy, setOderBy] = useState('asc');
+
+  const filteredAppointments = appointmentList.filter(item => {
+    return (
+      item.petName.toLowerCase().includes(query.toLowerCase()) ||
+      item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+      item.aptNotes.toLowerCase().includes(query.toLowerCase())
+    )
+  }).sort((a, b) => {
+    let order = (orderBy === 'asc') ? 1 : -1;
+
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 * order : 1 * order
+    )
+  });
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data);
+      })
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return (
+    <div className='App container mx-auto mt-3 font-thin'>
+      <h1 className='text-5xl mb-3'>
+        <BiCalendar className='inline-block text-red-400 align-top' />Your Appointments
+      </h1>
+
+      <AddAppointment
+        onSendAppointment={apt => setAppointmentList([...appointmentList, apt])}
+        lastId={appointmentList.reduce((max, item) =>  Number(item.id) > max ? Number(item.id) : max, 0 )}
+      />
+      <Search
+        query={query}
+        onQueryChange={query => setQuery(query)}
+
+        orderBy={orderBy}
+        onOrderByChange={order => setOderBy(order)}
+        sortBy={sortBy}
+        onSortByChange={sort => setSortBy(sort)}
+      />
+
+      <ul className='divide-y divide-gray-200'>
+        {filteredAppointments.map(appointment => (
+          <AppointmentInfo
+            key={appointment.id}
+            appointment={appointment}
+            onDeleteAppointment={appointmentId => handleRemoveItem(appointmentId, setAppointmentList, appointmentList)} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
